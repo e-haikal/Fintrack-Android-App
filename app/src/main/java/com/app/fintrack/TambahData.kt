db04fcc7ebba1df8a70e76cb1ea0479a318430a1
e-haikal <taufanasman@yahoo.com>
2024-07-01 19:46:36 +0700

    screen: fix layout tambah data fragment

--- app/src/main/java/com/app/fintrack/TambahData.kt
+++ app/src/main/java/com/app/fintrack/TambahData.kt
@@ -1,49 +1,71 @@
 package com.app.fintrack
 
 import PemasukanFragment
-import android.graphics.Color
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentContainerView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TambahData : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_tambah_data)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Initialize views
+        val tanggalEditText: EditText = findViewById(R.id.tanggalEditText)
+        val pilihTanggalImageView: ImageView = findViewById(R.id.pilihTanggalImageView)
+
+        // Calendar instance
+        val calendar = Calendar.getInstance()
+
+        // Date formatter
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+
+        // Set initial text on tanggalEditText
+        tanggalEditText.setText(dateFormat.format(calendar.time))
+
+        // Date picker dialog listener
+        val dateSetListener = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, monthOfYear)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            tanggalEditText.setText(dateFormat.format(calendar.time))
         }
 
-//        Kode item fragment start
+        // Set click listener on image view to show date picker dialog
+        pilihTanggalImageView.setOnClickListener {
+            DatePickerDialog(
+                this,
+                dateSetListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
+        // Fragment initialization and button click listeners
         val pemasukanButton: Button = findViewById(R.id.pemasukanButton)
         val pengeluaranButton: Button = findViewById(R.id.pengeluaranButton)
         val fc_konten: FragmentContainerView = findViewById(R.id.fc_konten)
         val tahitiColor = ContextCompat.getColor(this, R.color.tahiti_gold)
         val dustyGreyColor = ContextCompat.getColor(this, R.color.dusty_grey)
+
         pemasukanButton.setOnClickListener {
             pemasukanButton.setBackgroundColor(tahitiColor)
             pengeluaranButton.setBackgroundColor(dustyGreyColor)
-            val fm = supportFragmentManager
-            val ft = fm.beginTransaction()
-            ft.replace(fc_konten.id, PemasukanFragment())
-            ft.commit()
+            supportFragmentManager.beginTransaction().replace(fc_konten.id, PemasukanFragment()).commit()
         }
+
         pengeluaranButton.setOnClickListener {
             pemasukanButton.setBackgroundColor(dustyGreyColor)
             pengeluaranButton.setBackgroundColor(tahitiColor)
-            val fm = supportFragmentManager
-            val ft = fm.beginTransaction()
-            ft.replace(fc_konten.id, PengeluaranFragment())
-            ft.commit()
+            supportFragmentManager.beginTransaction().replace(fc_konten.id, PengeluaranFragment()).commit()
         }
-//        Kode item fragment end
     }
 }
