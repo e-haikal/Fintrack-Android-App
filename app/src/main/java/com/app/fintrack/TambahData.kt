f13239e08e0f073a002c2deb25506e44afd19804
e-haikal <taufanasman@yahoo.com>
2024-07-01 16:24:58 +0700

    screen: penyesuaian warna tab di tambah data

--- app/src/main/java/com/app/fintrack/TambahData.kt
+++ app/src/main/java/com/app/fintrack/TambahData.kt
@@ -1,9 +1,11 @@
 package com.app.fintrack
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentContainerView
@@ -23,13 +25,19 @@ class TambahData : AppCompatActivity() {
         val pemasukanButton: Button = findViewById(R.id.pemasukanButton)
         val pengeluaranButton: Button = findViewById(R.id.pengeluaranButton)
         val fc_konten: FragmentContainerView = findViewById(R.id.fc_konten)
+        val tahitiColor = ContextCompat.getColor(this, R.color.tahiti_gold)
+        val dustyGreyColor = ContextCompat.getColor(this, R.color.dusty_grey)
         pemasukanButton.setOnClickListener {
+            pemasukanButton.setBackgroundColor(tahitiColor)
+            pengeluaranButton.setBackgroundColor(dustyGreyColor)
             val fm = supportFragmentManager
             val ft = fm.beginTransaction()
             ft.replace(fc_konten.id, PemasukanFragment())
             ft.commit()
         }
         pengeluaranButton.setOnClickListener {
+            pemasukanButton.setBackgroundColor(dustyGreyColor)
+            pengeluaranButton.setBackgroundColor(tahitiColor)
             val fm = supportFragmentManager
             val ft = fm.beginTransaction()
             ft.replace(fc_konten.id, PengeluaranFragment())
