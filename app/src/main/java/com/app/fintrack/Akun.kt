97cf53ad5a37ef9e4f5090bf02bb9dd90e610ae5
e-haikal <taufanasman@yahoo.com>
2024-07-02 07:05:29 +0700

    screen: add navigasi home

--- app/src/main/java/com/app/fintrack/Akun.kt
+++ app/src/main/java/com/app/fintrack/Akun.kt
@@ -1,6 +1,9 @@
 package com.app.fintrack
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +19,45 @@ class Akun : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //mengaktifkan  akun menu
+        val faqMenu: TextView = findViewById(R.id.faqMenu)
+        faqMenu.setOnClickListener {
+            val pindah = Intent(this, Faq::class.java)
+            startActivity(pindah)
+        }
+
+        //mengaktifkan  tentang menu
+        val tentangMenu: TextView = findViewById(R.id.tentangMenu)
+        tentangMenu.setOnClickListener {
+            val pindah = Intent(this, Tentang::class.java)
+            startActivity(pindah)
+        }
+
+        //mengaktifkan  privasi menu
+        val privasiMenu: TextView = findViewById(R.id.privasiMenu)
+        privasiMenu.setOnClickListener {
+            val pindah = Intent(this, Privasi::class.java)
+            startActivity(pindah)
+        }
+
+        //mengaktifkan  akun menu
+        val ketentuanMenu: TextView = findViewById(R.id.ketentuanMenu)
+        ketentuanMenu.setOnClickListener {
+            val pindah = Intent(this, Ketentuan::class.java)
+            startActivity(pindah)
+        }
+
+        //mengaktifkan  logout button
+        val logoutButton: TextView = findViewById(R.id.logoutButton)
+        logoutButton.setOnClickListener {
+            val pindah = Intent(this, Login1::class.java)
+            startActivity(pindah)
+            finish()
+        }
+
+
+
 
     }
 }
